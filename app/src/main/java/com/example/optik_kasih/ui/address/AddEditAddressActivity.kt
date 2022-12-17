fe994fc7e4c8db60c0bedd4a663ea42de85cbde8
Kevin <kevinn@pmberjaya.com>
2022-12-18 02:35:36 +0700

    view done

---
+++ app/src/main/java/com/example/optik_kasih/ui/address/AddEditAddressActivity.kt
@@ -0,0 +1,12 @@
+package com.example.optik_kasih.ui.address
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.optik_kasih.R
+
+class AddEditAddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_address)
+    }
+}
---
+++ app/src/main/java/com/example/optik_kasih/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.optik_kasih.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.optik_kasih.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
